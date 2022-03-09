055286a68627a63f08fb7dad49439587bc5228a7
meritacenaj <meritushi@gmail.com>
2022-03-09 15:03:54 +0000

    OK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+
+
+
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+    const newValue =
+    {
+      value: newTodo,
+      completed: false, 
+    }
+    setTodos([...todos, newValue]);
+
+    setNewTodo("")
+  };
+  
+  const handleTodoDelete = (id) => {
+    setTodos(todos.filter((todo, i) => {
+      return id !== i;
+    }))
+}
+   const handleToggleComplete = (idx) => {
+
+       const updateTodos = todos.filter((todo,i) => {
+          if (idx !== i) {
+            
+            todo.completed = !todo.completed;
+
+          }
+           
+          return todo;
+          
+       });
+
+       setTodos(updateTodos);
+       
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <form
+         onSubmit={(event) => {handleNewTodoSubmit(event);}}>
+        <input onChange={(event) => {
+            setNewTodo(event.target.value);
+          }}
+          type="text"
+          value={newTodo}
+        />
+
+  
+        <div>
+            <button>Add</button>
+        </div>
+      </form> 
+
+      {todos.map((todo, i) => {
+        return (
+
+          <div key={i}>
+          
+          
+            <span>{todo.value}</span>
+
+            <input type="checkbox" checked={todo.completed} onChange={handleToggleComplete} />
+          {  
+
+              todo.completed ?
+            <p>Completed</p>:
+            'Not Completed'
+      }  
+
+
+            <button 
+                onClick={(event) => {
+                   handleTodoDelete(i);
+                }}
+                style={{ marginLeft: "10px"}}
+            >
+                Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
